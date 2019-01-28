fdabaa074f442166d427f8e62ca3aa662c5b03a4
franciscomacedobrito <franciscomacedobrito@gmail.com>
2019-01-28 12:43:26 +0000

    Update stream-list.module.ts

--- twitch-exercise/src/app/pages/stream-list/stream-list.module.ts
+++ twitch-exercise/src/app/pages/stream-list/stream-list.module.ts
@@ -2,7 +2,6 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { StreamCardModule } from '../../core/components/stream-card/stream-card.module';
 import { StreamListComponent } from './stream-list.component';
 
